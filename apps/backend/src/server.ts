8be639b09b8d8d1ea649c3ab884372540df881e0
Michelle Chan <mc.chellechan@gmail.com>
2024-09-15 04:56:39 +0800

    update server.ts

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -17,7 +17,8 @@ async function startServer() {
         console.log('Database connected and schema synchronized.');
 
         // Start the server
-        const app = new App([new SubmissionRoute(), new CompanyRoute(), new ContributionRoute(), new MilestoneRoute(), new UserRoute(), new UserMilestoneRoute()]);
+        const app = new App([new SubmissionRoute()]);
+        // const app = new App([new SubmissionRoute(), new CompanyRoute(), new ContributionRoute(), new MilestoneRoute(), new UserRoute(), new UserMilestoneRoute()]);
 
         app.listen();
         console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
