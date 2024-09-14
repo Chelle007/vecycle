30424ab8b227067e88051600396a4e1c668e782a
desraymondz <desraymondz@gmail.com>
2024-09-15 07:23:34 +0800

    company profile css enhance

---
+++ apps/frontend/CompanyProfile/company-profile.js
@@ -0,0 +1,31 @@
+const compProfileData = {
+    companyName: "DASANI",
+    bannerImgUrl: "https://www.beveragedaily.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/beveragedaily.com/article/2022/07/28/dasani-rolls-out-100-rpet-bottles-in-north-america/15635754-1-eng-GB/Dasani-rolls-out-100-rPET-bottles-in-North-America.jpg",
+    userToken: 75,
+    token_to_reach: 100,
+    get progressWidth() {
+        return (this.userToken / this.token_to_reach) * 100 + "%";
+    },
+    rewards: [
+        {
+            rewardName: "10% Discount",
+            statusText: "Completed",
+            statusIcon: "✅",
+            lockedClass: "" // put empty string if not locked
+        },
+        {
+            rewardName: "Free Drink",
+            statusText: "10 to go!",
+            statusIcon: "🔒",
+            lockedClass: "locked"
+        },
+        {
+            rewardName: "Exclusive deals",
+            statusText: "25 to go!",
+            statusIcon: "🔒",
+            lockedClass: "locked"
+        }
+    ]
+};
+
+generateHTMLObject("company-profile-template", compProfileData, "company-profile-container")
