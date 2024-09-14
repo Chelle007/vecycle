99876fcc1008537406ccc95feda1becfa327f093
desraymondz <desraymondz@gmail.com>
2024-09-15 07:36:22 +0800

    typo

--- apps/frontend/CompanyProfile/company-profile.js
+++ apps/frontend/CompanyProfile/company-profile.js
@@ -2,7 +2,7 @@ const compProfileData = {
     companyName: "DASANI",
     bannerImgUrl: "https://www.beveragedaily.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/beveragedaily.com/article/2022/07/28/dasani-rolls-out-100-rpet-bottles-in-north-america/15635754-1-eng-GB/Dasani-rolls-out-100-rPET-bottles-in-North-America.jpg",
     userToken: 75,
-    token_to_reach: 100,
+    token_to_reach: 10,
     get progressWidth() {
         return (this.userToken / this.token_to_reach) * 100 + "%";
     },
@@ -15,13 +15,13 @@ const compProfileData = {
         },
         {
             rewardName: "Free Drink",
-            statusText: "10 to go!",
+            statusText: "1 to go!",
             statusIcon: "🔒",
             lockedClass: "locked"
         },
         {
             rewardName: "Exclusive deals",
-            statusText: "25 to go!",
+            statusText: "2 to go!",
             statusIcon: "🔒",
             lockedClass: "locked"
         }
