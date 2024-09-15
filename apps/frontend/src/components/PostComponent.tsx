01f86e44f0fb1ebca2d459aeffdbb613844092d4
desraymondz <desraymondz@gmail.com>
2024-09-15 12:03:12 +0800

    final countdown

--- apps/frontend/src/components/PostComponent.tsx
+++ apps/frontend/src/components/PostComponent.tsx
@@ -1,47 +1,46 @@
-// import { DAppKitProvider } from "@vechain/dapp-kit-react";
-// import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
-// export { default as Dropzone } from './Dropzone';
-// export { default as UserInstructions } from './UserInstructions';
-// export { default as NavbarComponent } from './NavbarComponent';
-// export { default as SubmissionModal } from './SubmissionModal';
+import { DAppKitProvider } from "@vechain/dapp-kit-react";
+import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
+
+// components/index.js
+import { default as Dropzone } from './Dropzone';
+import { default as SubmissionModal } from './SubmissionModal';
+import { default as UserInstructions } from './UserInstructions';
 
 
-// function PostComponent() {
-//     return (
-//         <ChakraProvider>
-//             <DAppKitProvider
-//                 usePersistence
-//                 requireCertificate={false}
-//                 genesis="test"
-//                 nodeUrl="https://testnet.vechain.org/"
-//                 logLevel={"DEBUG"}
-//             >
-//                 {/* <Navbar /> */}
-//                 <Flex flex={1}>
-//                     <Container
-//                         mt={{ base: 4, md: 10 }}
-//                         maxW="container.xl"
-//                         mb={{ base: 4, md: 10 }}
-//                         display="flex"
-//                         flexDirection="column"
-//                         alignItems="center"
-//                         justifyContent="center"
-//                     >
-//                         {/* <InfoCard /> */}
-//                         {/* <Instructions /> */}
-//                         <Dropzone />
-//                         <UserInstructions />
-//                     </Container>
-//                 </Flex>
-//                 {/* <Footer /> */}
+function PostComponent() {
+  return (
+    <ChakraProvider >
+      <DAppKitProvider
+        usePersistence
+        requireCertificate={false}
+        genesis="test"
+        nodeUrl="https://testnet.vechain.org/"
+        logLevel={"DEBUG"}
+      >
+        {/* <Navbar /> */}
+        <Flex flex={1}>
+          <Container
+            mt={{ base: 4, md: 10 }}
+            maxW="container.xl"
+            mb={{ base: 4, md: 10 }}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            {/* <InfoCard /> */}
+            {/* <Instructions /> */}
+            <Dropzone />
+            <UserInstructions />
+          </Container>
+        </Flex>
+        {/* <Footer /> */}
 
-//                 {/* MODALS  */}
-//                 <SubmissionModal />
+        {/* MODALS  */}
+        <SubmissionModal />
+      </DAppKitProvider>
+    </ChakraProvider>
+  );
+}
 
-//                 <NavbarComponent path="/post" />
-//             </DAppKitProvider>
-//         </ChakraProvider>
-//     );
-// }
-
-// export default PostComponent;
+export default PostComponent;
